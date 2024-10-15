d6371caf3e036dff1f38877a78ed3793885c9339
Riancahyo <riancahyo75@gmail.com>
2024-10-15 11:39:31 +0700

    Perubahan

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,36 +3,39 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
+import com.example.happybirthday.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             HappyBirthdayTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    GreetingText(
-                        message = "Happy Birthday Repan!",
-                        from = "From Rian",
-                        modifier = Modifier.padding(8.dp)
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Rian",
+                        modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -41,34 +44,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String, modifier: Modifier = Modifier) {
     Column(
-        verticalArrangement = Arrangement.Center,
         modifier = modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(Color(0xFF42A5F5), Color.White)
+                )
+            )
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = message,
-            fontSize = 100.sp,
-            lineHeight = 116.sp,
-            textAlign = TextAlign.Center
-        )
-        Text(
-            text = from,
-            fontSize = 36.sp,
+        Image(
+            painter = painterResource(id = R.drawable.kue_ulang_tahun),
+            contentDescription = "Birthday Cake",
             modifier = Modifier
-                .padding(16.dp)
-                .align(alignment = Alignment.End)
-
+                .size(300.dp)
+                .padding(bottom = 16.dp)
+                .clip(CircleShape),
+            contentScale = ContentScale.Crop
         )
-
+        Text(
+            text = "Happy Birthday $name!",
+            color = Color(0xFF6200EA),
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = "\"Selamat ulang tahun!🎉🎂\n" +
+                    "Semoga setiap langkahmu dipenuhi kebahagiaan, kesuksesan, dan cinta. Teruslah bersinar dan menjadi pribadi yang inspiratif bagi banyak orang.",
+            color = Color(0xFF03A9F4),
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(top = 15.dp)
+        )
     }
-
 }
 
 @Preview(showBackground = true)
 @Composable
-fun BirthdayCardPreview() {
+fun GreetingPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Rahmad!", from = "From Brian")
+        Greeting("Android")
     }
 }
